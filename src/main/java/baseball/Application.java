48b8877c553c6b33c1b3e0d15a342089ac3b734b
yujeong <b.imnuj@gmail.com>
2022-10-11 17:30:58 +0900

    feat(baseball): 몇 개의 숫자가 같은지 알 수 있는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,13 +1,17 @@
 package baseball;
 
-import baseball.domain.NumberGenerator;
+import baseball.domain.Judgement;
 
-import java.util.List;
+import java.util.Arrays;
 
 public class Application {
     public static void main(String[] args) {
-        NumberGenerator numberGenerator = new NumberGenerator();
-        List<Integer> numbers = numberGenerator.createRandomNumbers();
-        System.out.println("numbers = " + numbers);
+        // NumberGenerator numberGenerator = new NumberGenerator();
+        // List<Integer> numbers = numberGenerator.createRandomNumbers();
+        // System.out.println("numbers = " + numbers);
+
+        Judgement judgement = new Judgement();
+        int count = judgement.correctCount(Arrays.asList(1, 2, 3), Arrays.asList(1, 2, 3));
+        System.out.println("count = " + count); // 3
     }
 }
--- src/main/java/baseball/domain/Judgement.java
+++ src/main/java/baseball/domain/Judgement.java
@@ -4,7 +4,16 @@ import java.util.List;
 
 public class Judgement {
     public int correctCount(List<Integer> computer, List<Integer> player) {
-        return 0;
+        int count = 0;
+
+        for (int i = 0; i < player.size(); i++) {
+            int playerNumber = player.get(i);
+            if (computer.contains(playerNumber)) {
+                count++;
+            }
+        }
+
+        return count;
     }
 
     public boolean hasPlace(int place, int number) {
