d45ec2e0bd2d39fe8811fd80e213094cece4382e
yujeong <b.imnuj@gmail.com>
2022-10-11 21:01:52 +0900

    refactor(baseball): @ParameterzizedTest를 사용한 테스트 중복 제거

--- src/test/java/baseball/domain/RefereeTest.java
+++ src/test/java/baseball/domain/RefereeTest.java
@@ -2,7 +2,8 @@ package baseball.domain;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.Arrays;
 import java.util.List;
@@ -16,27 +17,10 @@ class RefereeTest {
         referee = new Referee();
     }
 
-    @Test
-    void 스트라이크3() {
-        String result = referee.compare(COMPUTER, Arrays.asList(1, 2, 3));
-        Assertions.assertThat(result).isEqualTo("0 볼 3 스트라이크");
-    }
-
-    @Test
-    void 아웃() {
-        String result = referee.compare(COMPUTER, Arrays.asList(4, 5, 6));
-        Assertions.assertThat(result).isEqualTo("아웃");
-    }
-
-    @Test
-    void 볼3() {
-        String result = referee.compare(COMPUTER, Arrays.asList(3, 1, 2));
-        Assertions.assertThat(result).isEqualTo("3 볼 0 스트라이크");
-    }
-
-    @Test
-    void 볼2_스트라이크1() {
-        String result = referee.compare(COMPUTER, Arrays.asList(2, 1, 3));
-        Assertions.assertThat(result).isEqualTo("2 볼 1 스트라이크");
+    @ParameterizedTest
+    @CsvSource({"1, 2, 3, 0 볼 3 스트라이크", "4, 5, 6, 아웃", "3, 1, 2, 3 볼 0 스트라이크", "2, 1, 3, 2 볼 1 스트라이크"})
+    public void compare(int number1, int number2, int number3, String expected) {
+        String actual = referee.compare(COMPUTER, Arrays.asList(number1, number2, number3));
+        Assertions.assertThat(actual).isEqualTo(expected);
     }
 }
