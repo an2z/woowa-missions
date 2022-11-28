a8057770e5b6fe1da172b90db8e9e07cf90c1d13
anneu <b.imnuj@gmail.com>
2022-11-29 05:09:37 +0900

    feat(BaseBallGame): 컴퓨터를 생성하는 기능 구현

--- java-baseball/src/main/java/baseball/domain/BaseBallGame.java
+++ java-baseball/src/main/java/baseball/domain/BaseBallGame.java
@@ -6,11 +6,16 @@ public class BaseBallGame {
 
     private final NumbersGenerator numbersGenerator = new NumbersGenerator();
 
+    private Computer computer;
     private Player player;
 
     public Player makePlayer(String input) {
         List<Integer> playerNumbers = numbersGenerator.makePlayer(input);
         player = new Player(playerNumbers);
         return player;
+    }
+
+    public void makeComputer() {
+        computer = new Computer(numbersGenerator.makeComputer());
     }
 }
