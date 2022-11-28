2199814a8af3b980a3e443083c6a0abad52c479b
anneu <b.imnuj@gmail.com>
2022-11-29 05:19:02 +0900

    test(NumbersGeneratorTest): 플레이어 수를 생성하는 기능 테스트

--- java-baseball/src/test/java/baseball/domain/NumbersGeneratorTest.java
+++ java-baseball/src/test/java/baseball/domain/NumbersGeneratorTest.java
@@ -2,9 +2,12 @@ package baseball.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class NumbersGeneratorTest {
 
@@ -16,5 +19,18 @@ class NumbersGeneratorTest {
         List<Integer> numbers = generator.makeComputer();
         int actual = numbers.size();
         assertThat(actual).isEqualTo(3);
+    }
+
+    @DisplayName("플레이어 수가 생성된다.")
+    @CsvSource({
+            "123, 1, 2, 3",
+            "456, 4, 5, 6",
+            "789, 7, 8, 9"
+    })
+    @ParameterizedTest
+    void makePlayer(String input, int number1, int number2, int number3) {
+        List<Integer> numbers = generator.makePlayer(input);
+        List<Integer> expected = Arrays.asList(number1, number2, number3);
+        assertThat(numbers).isEqualTo(expected);
     }
 }
