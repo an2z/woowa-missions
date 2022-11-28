2b8b5fc3c6bcd28cf1dfeccd99931d8c40c086e7
anneu <b.imnuj@gmail.com>
2022-11-28 17:58:33 +0900

    test(PlayerTest): 3자리가 아닐 경우 예외를 발생시키는 검증 기능 테스트

--- java-baseball/src/test/java/baseball/domain/PlayerTest.java
+++ java-baseball/src/test/java/baseball/domain/PlayerTest.java
@@ -5,10 +5,19 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 class PlayerTest {
+
+    @DisplayName("3자리 수가 아닐 경우 예외 발생 확인")
+    @Test
+    void makeNumbersWrongSize() {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4);
+        assertThrows(IllegalArgumentException.class,
+                () -> new Player(numbers));
+    }
 
     @DisplayName("각 자리 숫자가 1부터 9까지의 숫자가 아닐 경우 예외 발생 확인")
     @CsvSource({
