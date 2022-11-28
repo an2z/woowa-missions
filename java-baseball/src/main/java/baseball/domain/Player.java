12a773bd4217e61e8bf27617d51b76329b68b1f5
anneu <b.imnuj@gmail.com>
2022-11-28 17:37:01 +0900

    feat(Player): 각 자리 숫자가 1부터 9까지의 수가 아닐 경우 예외를 발생시키는 검증 기능 구현

--- java-baseball/src/main/java/baseball/domain/Player.java
+++ java-baseball/src/main/java/baseball/domain/Player.java
@@ -8,7 +8,24 @@ public class Player {
     private final List<Integer> numbers;
 
     public Player(List<Integer> numbers) {
+        validate(numbers);
         this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        validateRange(numbers);
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (isOutOfRange(number)) {
+                throw new IllegalArgumentException("[ERROR] 각 자리 숫자는 1부터 9까지의 숫자만 가능합니다.");
+            }
+        }
+    }
+
+    private boolean isOutOfRange(int number) {
+        return number < 1 || 9 < number;
     }
 
     @Override
