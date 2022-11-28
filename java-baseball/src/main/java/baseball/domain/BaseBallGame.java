2b1d419aa6d8c4ff250bfcea8cc0a787f1b431a5
anneu <b.imnuj@gmail.com>
2022-11-29 05:21:01 +0900

    refactor(BaseBallGame): 플레이어를 생성하는 기능의 반환 타입 변경

--- java-baseball/src/main/java/baseball/domain/BaseBallGame.java
+++ java-baseball/src/main/java/baseball/domain/BaseBallGame.java
@@ -1,7 +1,5 @@
 package baseball.domain;
 
-import java.util.List;
-
 public class BaseBallGame {
 
     private final NumbersGenerator numbersGenerator = new NumbersGenerator();
@@ -9,10 +7,8 @@ public class BaseBallGame {
     private Computer computer;
     private Player player;
 
-    public Player makePlayer(String input) {
-        List<Integer> playerNumbers = numbersGenerator.makePlayer(input);
-        player = new Player(playerNumbers);
-        return player;
+    public void makePlayer(String input) {
+        player = new Player(numbersGenerator.makePlayer(input));
     }
 
     public void makeComputer() {
