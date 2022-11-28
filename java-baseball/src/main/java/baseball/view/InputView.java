de27931a5a2a37508e610b9b05805565f894e334
anneu <b.imnuj@gmail.com>
2022-11-29 03:39:13 +0900

    feat(InputView): 숫자 형식이 아닐 경우 예외 발생 검증 기능 구현

--- java-baseball/src/main/java/baseball/view/InputView.java
+++ java-baseball/src/main/java/baseball/view/InputView.java
@@ -3,12 +3,23 @@ package baseball.view;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputView {
+
     public String readPlayerNumbers() {
-        return input();
+        String input = input();
+        validateNumberFormat(input);
+        return input;
     }
 
     public String readGameCommand() {
         return input();
+    }
+
+    private void validateNumberFormat(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자 형식만 가능합니다.");
+        }
     }
 
     private String input() {
