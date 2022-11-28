9004642dd2f7401e34ffbb606476d5fdb5664aac
anneu <b.imnuj@gmail.com>
2022-11-28 17:45:12 +0900

    test(PlayerTest): 각 자리 숫자가 1부터 9까지의 수가 아닐 경우 예외를 발생시키는 검증 기능 테스트

---
+++ java-baseball/src/test/java/baseball/domain/PlayerTest.java
@@ -0,0 +1,25 @@
+package baseball.domain;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class PlayerTest {
+
+    @DisplayName("각 자리 숫자가 1부터 9까지의 숫자가 아닐 경우 예외 발생 확인")
+    @CsvSource({
+            "0, 1, 2",
+            "1, 0 ,2",
+            "1, 2, 0"
+    })
+    @ParameterizedTest
+    void makeNumbersOutOfRange(int number1, int number2, int number3) {
+        List<Integer> numbers = Arrays.asList(number1, number2, number3);
+        assertThrows(IllegalArgumentException.class,
+                () -> new Player(numbers));
+    }
+}
