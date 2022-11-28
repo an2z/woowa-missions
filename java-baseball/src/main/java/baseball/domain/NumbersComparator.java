fb6202c949b4dc62a2d91ea1dc5184ca295c5696
anneu <b.imnuj@gmail.com>
2022-11-28 16:58:03 +0900

    feat(NumbersComparator): 컴퓨터 수와 플레이어 수를 비교해 몇 개의 수가 같은 자리에 있는지 구하는 기능 구현

--- java-baseball/src/main/java/baseball/domain/NumbersComparator.java
+++ java-baseball/src/main/java/baseball/domain/NumbersComparator.java
@@ -9,4 +9,20 @@ public class NumbersComparator {
                 .filter(computer::contains)
                 .count();
     }
+
+    public int compareSamePlace(List<Integer> computer, List<Integer> player) {
+        int samePlaceCount = 0;
+        for (int place = 0; place < computer.size(); place++) {
+            if (isSamePlace(place, computer, player)) {
+                samePlaceCount++;
+            }
+        }
+        return samePlaceCount;
+    }
+
+    private boolean isSamePlace(int place, List<Integer> computer, List<Integer> player) {
+        Integer computerNumber = computer.get(place);
+        Integer playerNumber = player.get(place);
+        return playerNumber.equals(computerNumber);
+    }
 }
