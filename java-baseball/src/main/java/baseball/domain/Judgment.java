99cd06efd12face3e9967a9ee7c3814fbd77a2b4
anneu <b.imnuj@gmail.com>
2022-11-29 04:36:12 +0900

    refactor(Judgment): 컬렉션이 아닌 객체를 사용하도록 매개변수 타입 수정

--- java-baseball/src/main/java/baseball/domain/Judgment.java
+++ java-baseball/src/main/java/baseball/domain/Judgment.java
@@ -1,14 +1,12 @@
 package baseball.domain;
 
-import java.util.List;
-
 public class Judgment {
     private final NumbersComparator comparator = new NumbersComparator();
 
     private int ball;
     private int strike;
 
-    public void judge(List<Integer> computer, List<Integer> player) {
+    public void judge(Computer computer, Player player) {
         ball = comparator.compareSameNumber(computer, player);
         int samePlaceCount = comparator.compareSamePlace(computer, player);
         if (samePlaceCount > 0) {
