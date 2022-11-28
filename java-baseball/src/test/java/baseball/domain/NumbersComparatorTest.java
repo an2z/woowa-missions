d8de2586b776f69b32213d92a187cdc2e89f787b
anneu <b.imnuj@gmail.com>
2022-11-29 04:32:19 +0900

    refactor(NumbersComparatorTest): 컬렉션이 아닌 객체를 사용하도록 테스트 수정

--- java-baseball/src/test/java/baseball/domain/NumbersComparatorTest.java
+++ java-baseball/src/test/java/baseball/domain/NumbersComparatorTest.java
@@ -3,7 +3,6 @@ package baseball.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Arrays;
-import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -13,11 +12,11 @@ class NumbersComparatorTest {
 
     private final NumbersComparator comparator = new NumbersComparator();
 
-    private List<Integer> computer;
+    private Computer computer;
 
     @BeforeEach
     void setUp() {
-        computer = Arrays.asList(1, 2, 3);
+        computer = new Computer(Arrays.asList(1, 2, 3));
     }
 
     @DisplayName("몇 개의 수가 같은지 비교한다.")
@@ -29,7 +28,7 @@ class NumbersComparatorTest {
     })
     @ParameterizedTest
     void compareSameNumber(int number1, int number2, int number3, int expected) {
-        List<Integer> player = Arrays.asList(number1, number2, number3);
+        Player player = new Player(Arrays.asList(number1, number2, number3));
         int actual = comparator.compareSameNumber(computer, player);
         assertThat(actual).isEqualTo(expected);
     }
@@ -43,7 +42,7 @@ class NumbersComparatorTest {
     })
     @ParameterizedTest
     void compareSamePlace(int number1, int number2, int number3, int expected) {
-        List<Integer> player = Arrays.asList(number1, number2, number3);
+        Player player = new Player(Arrays.asList(number1, number2, number3));
         int actual = comparator.compareSamePlace(computer, player);
         assertThat(actual).isEqualTo(expected);
     }
