27cf513061bc72ff8fdef53397243efad08d16c5
anneu <b.imnuj@gmail.com>
2022-11-28 15:27:13 +0900

    feat(OutputView): 게임 재시작/종료 여부 입력 요청 문구 출력 기능 구현

--- java-baseball/src/main/java/baseball/view/OutputView.java
+++ java-baseball/src/main/java/baseball/view/OutputView.java
@@ -12,4 +12,8 @@ public class OutputView {
     public void printGameSuccess() {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
+
+    public void printGameCommandInputMsg() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
 }
