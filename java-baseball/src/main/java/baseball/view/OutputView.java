84990c7b40f228fb14ad48a1545f4e0d115773b7
anneu <b.imnuj@gmail.com>
2022-11-29 03:32:09 +0900

    fix(OutputView): 숫자 입력 요청 문구 출력 시 줄바꿈 하지 않도록 변경

--- java-baseball/src/main/java/baseball/view/OutputView.java
+++ java-baseball/src/main/java/baseball/view/OutputView.java
@@ -6,7 +6,7 @@ public class OutputView {
     }
 
     public void printNumbersInputMsg() {
-        System.out.println("숫자를 입력해주세요 : ");
+        System.out.print("숫자를 입력해주세요 : ");
     }
 
     public void printGameSuccess() {
