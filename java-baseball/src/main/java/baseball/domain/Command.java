ae46c84294c8d2733296d03fb366b95c1ea213ca
anneu <b.imnuj@gmail.com>
2022-11-29 17:12:23 +0900

    refactor(Command): 하드코딩 값을 상수 처리

--- java-baseball/src/main/java/baseball/domain/Command.java
+++ java-baseball/src/main/java/baseball/domain/Command.java
@@ -2,6 +2,10 @@ package baseball.domain;
 
 public class Command {
 
+    private static final String STOP = "2";
+    private static final String RESTART = "1";
+    private static final String COMMAND_ERROR = "[ERROR] " + RESTART + " 또는 " + STOP + "만 가능합니다.";
+
     private final String status;
 
     public Command(String status) {
@@ -10,16 +14,16 @@ public class Command {
     }
 
     public boolean restarts() {
-        return status.equals("1");
+        return status.equals(RESTART);
     }
 
     private void validate(String status) {
         if (isWrongStatus(status)) {
-            throw new IllegalArgumentException("[ERROR] 1 또는 2만 가능합니다.");
+            throw new IllegalArgumentException(COMMAND_ERROR);
         }
     }
 
     private boolean isWrongStatus(String status) {
-        return !(status.equals("1") || status.equals("2"));
+        return !(status.equals(RESTART) || status.equals(STOP));
     }
 }
