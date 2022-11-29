ed6f8d278eca13d1e0a556cd207db6e7c13218be
anneu <b.imnuj@gmail.com>
2022-11-29 17:09:05 +0900

    refactor(OutputView): 하드코딩 값을 상수 처리

--- java-baseball/src/main/java/baseball/view/OutputView.java
+++ java-baseball/src/main/java/baseball/view/OutputView.java
@@ -4,20 +4,29 @@ import baseball.domain.Judgment;
 
 public class OutputView {
 
+    private static final String BLANK = " ";
+    private static final String BALL = "볼";
+    private static final String NOTHING = "낫싱";
+    private static final String STRIKE = "스트라이크";
+    private static final String GAME_START = "숫자 야구 게임을 시작합니다.";
+    private static final String PLAYER_NUMBERS_INPUT = "숫자를 입력해주세요 : ";
+    private static final String GAME_SUCCESS = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String GAME_COMMAND_INPUT = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
     public void printGameStart() {
-        System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.println(GAME_START);
     }
 
     public void printNumbersInputMsg() {
-        System.out.print("숫자를 입력해주세요 : ");
+        System.out.print(PLAYER_NUMBERS_INPUT);
     }
 
     public void printGameSuccess() {
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println(GAME_SUCCESS);
     }
 
     public void printGameCommandInputMsg() {
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        System.out.println(GAME_COMMAND_INPUT);
     }
 
     public void printGameResult(Judgment judgment) {
@@ -28,14 +37,14 @@ public class OutputView {
 
     private String makeGameResult(int ball, int strike) {
         if (ball == 0 && strike == 0) {
-            return "낫싱";
+            return NOTHING;
         }
         if (ball == 0) {
-            return strike + "스트라이크";
+            return strike + STRIKE;
         }
         if (strike == 0) {
-            return ball + "볼";
+            return ball + BALL;
         }
-        return ball + "볼 " + strike + "스트라이크";
+        return ball + BALL + BLANK + strike + STRIKE;
     }
 }
