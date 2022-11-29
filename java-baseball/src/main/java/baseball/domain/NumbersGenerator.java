cab77cb68908a4a22f3d3362d827b4b172ce4984
anneu <b.imnuj@gmail.com>
2022-11-29 17:15:24 +0900

    refactor(NumbersGenerator): 하드코딩 값을 상수 처리

--- java-baseball/src/main/java/baseball/domain/NumbersGenerator.java
+++ java-baseball/src/main/java/baseball/domain/NumbersGenerator.java
@@ -6,10 +6,14 @@ import java.util.List;
 
 public class NumbersGenerator {
 
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 9;
+    private static final int NUMBERS_SIZE = 3;
+
     public List<Integer> makeComputerNumbers() {
         List<Integer> computer = new ArrayList<>();
-        while (computer.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
+        while (computer.size() < NUMBERS_SIZE) {
+            int randomNumber = Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
             if (!computer.contains(randomNumber)) {
                 computer.add(randomNumber);
             }
