78cef5f6233dc6581e35e55295ed647eb38a73c8
anneu <b.imnuj@gmail.com>
2022-11-29 15:15:20 +0900

    fix(OutputView): 0볼 또는 0스트리이크일 경우 결과가 함께 출력되는 오류 해결

--- java-baseball/src/main/java/baseball/view/OutputView.java
+++ java-baseball/src/main/java/baseball/view/OutputView.java
@@ -30,10 +30,10 @@ public class OutputView {
         if (ball == 0 && strike == 0) {
             return "낫싱";
         }
-        if (strike == 3) {
+        if (ball == 0) {
             return strike + "스트라이크";
         }
-        if (ball == 3) {
+        if (strike == 0) {
             return ball + "볼";
         }
         return ball + "볼 " + strike + "스트라이크";
