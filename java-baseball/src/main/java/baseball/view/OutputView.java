2724306c7b1662163befa46ca846e4f61f74db3f
anneu <b.imnuj@gmail.com>
2022-11-29 14:37:07 +0900

    feat(OutputView): 게임 결과를 출력하는 기능 구현

--- java-baseball/src/main/java/baseball/view/OutputView.java
+++ java-baseball/src/main/java/baseball/view/OutputView.java
@@ -1,5 +1,7 @@
 package baseball.view;
 
+import baseball.domain.Judgment;
+
 public class OutputView {
     public void printGameStart() {
         System.out.println("숫자 야구 게임을 시작합니다.");
@@ -15,5 +17,24 @@ public class OutputView {
 
     public void printGameCommandInputMsg() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+
+    public void printGameResult(Judgment judgment) {
+        int ball = judgment.getBall();
+        int strike = judgment.getStrike();
+        System.out.println(makeGameResult(ball, strike));
+    }
+
+    private String makeGameResult(int ball, int strike) {
+        if (ball == 0 && strike == 0) {
+            return "낫싱";
+        }
+        if (strike == 3) {
+            return strike + "스트라이크";
+        }
+        if (ball == 3) {
+            return ball + "볼";
+        }
+        return ball + "볼 " + strike + "스트라이크";
     }
 }
