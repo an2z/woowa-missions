8940d70c0fd300827cd0168a8d49362d4bc259b1
anneu <b.imnuj@gmail.com>
2022-11-29 16:36:58 +0900

    feat(Command): 게임 명령 상태가 1 또는 2가 아닐 경우 예외를 발생시키는 기능 구현

--- java-baseball/src/main/java/baseball/domain/Command.java
+++ java-baseball/src/main/java/baseball/domain/Command.java
@@ -5,6 +5,17 @@ public class Command {
     private final String status;
 
     public Command(String status) {
+        validate(status);
         this.status = status;
+    }
+
+    private void validate(String status) {
+        if (isWrongStatus(status)) {
+            throw new IllegalArgumentException("[ERROR] 1 또는 2만 가능합니다.");
+        }
+    }
+
+    private boolean isWrongStatus(String status) {
+        return !(status.equals("1") || status.equals("2"));
     }
 }
