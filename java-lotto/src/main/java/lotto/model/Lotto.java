9fd22649d7007d9f81092e3dc782a726a8c5a225
anneu <b.imnuj@gmail.com>
2022-12-04 00:20:56 +0900

    feat(Lotto): 로또 번호에 1~45 범위를 벗어난 숫자가 있는지 확인하는 기능 구현

--- java-lotto/src/main/java/lotto/model/Lotto.java
+++ java-lotto/src/main/java/lotto/model/Lotto.java
@@ -3,13 +3,17 @@ package lotto.model;
 import java.util.List;
 
 public class Lotto {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
     private static final int DEFAULT_SIZE = 6;
     private static final String SIZE_ERROR = "[ERROR] 로또 번호가 %d개가 아닙니다.";
+    private static final String RANGE_ERROR = "[ERROR] 로또 번호에 %d ~ %d 범위를 벗어난 숫자가 존재합니다.";
 
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validateSize(numbers);
+        validateRange(numbers);
         this.numbers = numbers;
     }
 
@@ -17,5 +21,17 @@ public class Lotto {
         if (numbers.size() != DEFAULT_SIZE) {
             throw new IllegalArgumentException(String.format(SIZE_ERROR, DEFAULT_SIZE));
         }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (!isValidRange(number)) {
+                throw new IllegalArgumentException(String.format(RANGE_ERROR, MIN_NUMBER, MAX_NUMBER));
+            }
+        }
+    }
+
+    private boolean isValidRange(int number) {
+        return MIN_NUMBER <= number && number <= MAX_NUMBER;
     }
 }
