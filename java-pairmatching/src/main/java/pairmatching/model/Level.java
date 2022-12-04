5c5c88d4c224a83cf178bdddf4a7ad68521c41ee
anneu <b.imnuj@gmail.com>
2022-12-04 16:34:00 +0900

    fix(Level): NPE 오류 발생 해결

--- java-pairmatching/src/main/java/pairmatching/model/Level.java
+++ java-pairmatching/src/main/java/pairmatching/model/Level.java
@@ -1,14 +1,15 @@
 package pairmatching.model;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public enum Level {
     LEVEL1("레벨1", List.of("자동차경주", "로또", "숫자야구게임")),
     LEVEL2("레벨2", List.of("장바구니", "결제", "지하철노선도")),
-    LEVEL3("레벨3"),
+    LEVEL3("레벨3", new ArrayList<>()),
     LEVEL4("레벨4", List.of("성능개선", "배포")),
-    LEVEL5("레벨5");
+    LEVEL5("레벨5", new ArrayList<>());
 
     private static final String LEVEL_ERROR = "[ERROR] 올바르지 않은 레벨입니다.";
 
