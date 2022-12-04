b77aa72d57c4c845e40109258dd8de3fd8b488ea
anneu <b.imnuj@gmail.com>
2022-12-04 14:47:49 +0900

    feat(Function): 존재하는 기능일 경우 가져오고, 존재하지 않는 기능일 경우 예외 발생 기능 구현

--- java-pairmatching/src/main/java/pairmatching/model/Function.java
+++ java-pairmatching/src/main/java/pairmatching/model/Function.java
@@ -1,14 +1,25 @@
 package pairmatching.model;
 
+import java.util.Arrays;
+
 public enum Function {
     MATCHING("1"),
     FIND("2"),
     INIT("3"),
     QUIT("Q");
 
+    private static final String SIGN_ERROR = "[ERROR] 1, 2, 3, Q 중에서 선택해주세요.";
+
     private String sign;
 
     Function(String sign) {
         this.sign = sign;
+    }
+
+    public static Function getBySign(String sign) {
+        return Arrays.stream(Function.values())
+                .filter(value -> sign.equals(value.sign))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException(SIGN_ERROR));
     }
 }
