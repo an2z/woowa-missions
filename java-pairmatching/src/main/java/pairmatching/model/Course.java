a890e430e3353e228d109da5e85301bf899bddbd
anneu <b.imnuj@gmail.com>
2022-12-04 16:48:27 +0900

    feat(Course): 존재하는 과정일 경우 가져오고, 존재하지 않는 과정일 경우 예외 발생 기능 구현

--- java-pairmatching/src/main/java/pairmatching/model/Course.java
+++ java-pairmatching/src/main/java/pairmatching/model/Course.java
@@ -1,12 +1,23 @@
 package pairmatching.model;
 
+import java.util.Arrays;
+
 public enum Course {
     BACKEND("백엔드"),
     FRONTEND("프론트엔드");
+
+    private static final String COURSE_ERROR = "[ERROR] 올바르지 않은 과정입니다.";
 
     private String type;
 
     Course(String type) {
         this.type = type;
+    }
+
+    public static Course getByType(String type) {
+        return Arrays.stream(Course.values())
+                .filter(value -> type.equals(value.type))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException(COURSE_ERROR));
     }
 }
