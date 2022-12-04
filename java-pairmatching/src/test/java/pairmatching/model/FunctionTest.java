6b356c1d772b409c68ceb555093fb5459b05da76
anneu <b.imnuj@gmail.com>
2022-12-04 16:37:42 +0900

    test(Function): 주어진 기호에 맞는 기능이 반환됨을 확인하는 테스트 추가

--- java-pairmatching/src/test/java/pairmatching/model/FunctionTest.java
+++ java-pairmatching/src/test/java/pairmatching/model/FunctionTest.java
@@ -1,5 +1,6 @@
 package pairmatching.model;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
@@ -15,5 +16,18 @@ class FunctionTest {
         assertThatThrownBy(() -> Function.getBySign(sign))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageStartingWith("[ERROR]");
+    }
+
+    @DisplayName("주어진 문자열에 해당하는 기능이 선택되는지 확인")
+    @CsvSource({
+            "1, MATCHING",
+            "2, FIND",
+            "3, INIT",
+            "Q, QUIT"
+    })
+    @ParameterizedTest
+    void makeFunctionBySign(String sign, Function expected) {
+        Function actual = Function.getBySign(sign);
+        assertThat(actual).isEqualTo(expected);
     }
 }
