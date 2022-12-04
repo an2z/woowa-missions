bcd42f7fa2d06bc23515b7bf9054aaa7542941d5
anneu <b.imnuj@gmail.com>
2022-12-04 18:57:07 +0900

    feat(Output): 기능 선택 요청 메세지 출력 기능 구현

---
+++ java-pairmatching/src/main/java/pairmatching/view/Output.java
@@ -0,0 +1,9 @@
+package pairmatching.view;
+
+public class Output {
+    private static final String FUNCTION_SELECTION = "기능을 선택하세요.";
+
+    public void printFunctionSelection() {
+        System.out.println(FUNCTION_SELECTION);
+    }
+}
