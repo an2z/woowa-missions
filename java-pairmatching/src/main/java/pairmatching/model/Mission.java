00669e7d0be4c4e5c22c550606d1472d63011977
anneu <b.imnuj@gmail.com>
2022-12-05 23:06:43 +0900

    feat(Mission): 미션 enum 생성

---
+++ java-pairmatching/src/main/java/pairmatching/model/Mission.java
@@ -0,0 +1,18 @@
+package pairmatching.model;
+
+public enum Mission {
+    RACING("자동차경주"), LOTTO("로또"), BASEBALL("숫자야구게임"),
+    BASKET("장바구니"), PAYMENT("결제"), SUBWAY("지하철노선도"),
+    PERFORMANCE("성능개선"), DISTRIBUTION("배포");
+
+    private final String name;
+
+    Mission(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
