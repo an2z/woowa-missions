d6c0a27211e24fa19e5a3dc37a2d5a4b9dc20e08
anneu <b.imnuj@gmail.com>
2022-12-09 16:40:43 +0900

    refactor(Output): 상수명 변경

--- java-lotto/src/main/java/lotto/view/Output.java
+++ java-lotto/src/main/java/lotto/view/Output.java
@@ -4,11 +4,11 @@ import lotto.model.Lotto;
 import lotto.model.Lottos;
 
 public class Output {
-    private static final String REQUEST_MONEY = "구입금액을 입력해 주세요.";
+    private static final String REQUEST_AMOUNT = "구입금액을 입력해 주세요.";
     private static final String PURCHASE_SIZE = "%n%d개를 구매했습니다.";
 
     public void requestAmount() {
-        System.out.println(REQUEST_MONEY);
+        System.out.println(REQUEST_AMOUNT);
     }
 
     public void printLotto(Lottos lottos) {
