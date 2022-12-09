bb95b024a47fca750caf2f50cf5482596e6b22a3
anneu <b.imnuj@gmail.com>
2022-12-09 14:32:36 +0900

    test(Money): 금액이 1000원 단위가 아니거나 1000원 미만인지 확인하는 기능 테스트

---
+++ java-lotto/src/test/java/lotto/model/MoneyTest.java
@@ -0,0 +1,24 @@
+package lotto.model;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class MoneyTest {
+    @DisplayName("1000원 미만일 경우 예외 발생 확인")
+    @Test
+    void createMoneyBySmallAmount() {
+        assertThatThrownBy(() -> new Money(900))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageStartingWith("[ERROR]");
+    }
+
+    @DisplayName("1000원 단위가 아닐 경우 예외 발생 확인")
+    @Test
+    void createMoneyByWrongUnit() {
+        assertThatThrownBy(() -> new Money(1100))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageStartingWith("[ERROR]");
+    }
+}
