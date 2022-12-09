c71b6e2bf65ae4705f403ac085b6c37e2c2c42f4
anneu <b.imnuj@gmail.com>
2022-12-09 16:32:09 +0900

    test(Money): 금액이 1000원 단위로 몇개인지 계산하는 기능 테스트

--- java-lotto/src/test/java/lotto/model/MoneyTest.java
+++ java-lotto/src/test/java/lotto/model/MoneyTest.java
@@ -5,6 +5,8 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class MoneyTest {
     @DisplayName("1000원 미만일 경우 예외 발생 확인")
@@ -28,5 +30,19 @@ class MoneyTest {
     void createMoney() {
         Money money = new Money(1000);
         assertThat(money).isEqualTo(new Money(1000));
+    }
+
+    @DisplayName("1000원이 몇개인지 계산한다.")
+    @CsvSource({
+            "1000, 1",
+            "2000, 2",
+            "10000, 10",
+            "15000, 15"
+    })
+    @ParameterizedTest
+    void countMoney(int amount, int expected) {
+        Money money = new Money(amount);
+        int actual = money.count();
+        assertThat(actual).isEqualTo(expected);
     }
 }
