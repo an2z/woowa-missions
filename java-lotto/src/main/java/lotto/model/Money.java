f95405d91af7095b1532393c095ac3a4a87d37d8
anneu <b.imnuj@gmail.com>
2022-12-09 14:34:03 +0900

    feat(Money): equals, hashCode 재정의

--- java-lotto/src/main/java/lotto/model/Money.java
+++ java-lotto/src/main/java/lotto/model/Money.java
@@ -1,5 +1,7 @@
 package lotto.model;
 
+import java.util.Objects;
+
 public class Money {
     private static final int MIN_MONEY = 1000;
     private static final String MONEY_ERROR = "[ERROR] %d원 단위만 가능합니다.";
@@ -23,5 +25,22 @@ public class Money {
 
     private static boolean isWrongUnit(int amount) {
         return amount % MIN_MONEY != 0;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Money money = (Money) o;
+        return amount == money.amount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount);
     }
 }
