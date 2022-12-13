683e877fb4919ebb2eaa35021d16f537fe94b814
anneu <b.imnuj@gmail.com>
2022-12-13 19:27:15 +0900

    test(Lotto): 로또 번호에 있는 숫자인지 확인하는 테스트 추가

--- java-lotto/src/test/java/lotto/model/LottoTest.java
+++ java-lotto/src/test/java/lotto/model/LottoTest.java
@@ -1,10 +1,14 @@
 package lotto.model;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -27,5 +31,19 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR]");
+    }
+
+    @DisplayName("로또 번호에 있는 숫자인지 확인한다.")
+    @CsvSource({
+            "1, true",
+            "2, true",
+            "9, false",
+            "45, false"
+    })
+    @ParameterizedTest
+    void checkContainsNumber(int number, boolean expected) {
+        Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+        boolean actual = lotto.isExist(number);
+        assertThat(actual).isEqualTo(expected);
     }
 }
