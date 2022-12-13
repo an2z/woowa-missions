450891a3e9a92b434d3fc29f0ef4140177687bf1
anneu <b.imnuj@gmail.com>
2022-12-13 19:32:48 +0900

    test(Bonus): 보너스 번호가 로또 번호에 있는 번호인지 확인하는 테스트 추가

--- java-lotto/src/test/java/lotto/model/BonusTest.java
+++ java-lotto/src/test/java/lotto/model/BonusTest.java
@@ -1,9 +1,13 @@
 package lotto.model;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.Arrays;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class BonusTest {
 
@@ -13,5 +17,24 @@ class BonusTest {
         assertThatThrownBy(() -> new Bonus(46))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageStartingWith("[ERROR]");
+    }
+
+    @DisplayName("₩")
+    @CsvSource({
+            "1, true",
+            "2, true",
+            "3, true",
+            "7, false",
+            "8, false",
+            "45, false"
+    })
+    @ParameterizedTest
+    void checkSameNumber(int number, boolean expected) {
+        Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+        Bonus bonus = new Bonus(number);
+
+        boolean actual = bonus.isExist(lotto);
+
+        assertThat(actual).isEqualTo(expected);
     }
 }
