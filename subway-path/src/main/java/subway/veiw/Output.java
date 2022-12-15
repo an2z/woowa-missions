3880c7b29fd9d373a15a37f1513e949df5120a41
anneu <b.imnuj@gmail.com>
2022-12-15 17:03:55 +0900

    feat: 메인 화면을 출력하는 기능 구현

---
+++ subway-path/src/main/java/subway/veiw/Output.java
@@ -0,0 +1,9 @@
+package subway.veiw;
+
+public class Output {
+    private static final String MAIN = "## 메인 화면%n1. 경로 조회%nQ. 종료%n";
+
+    public void printMain() {
+        System.out.printf(MAIN);
+    }
+}
