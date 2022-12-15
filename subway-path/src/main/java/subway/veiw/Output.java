ac8f7389159226a200747b51ccf258a1313dabe2
anneu <b.imnuj@gmail.com>
2022-12-15 17:21:34 +0900

    feat: 출발역, 도착역 입력 요청 메세지를 출력하는 기능 구현

--- subway-path/src/main/java/subway/veiw/Output.java
+++ subway-path/src/main/java/subway/veiw/Output.java
@@ -4,6 +4,8 @@ public class Output {
     private static final String MAIN = "## 메인 화면%n1. 경로 조회%nQ. 종료%n";
     private static final String CHOICE = "%n## 원하는 기능을 선택하세요.%n";
     private static final String STANDARD = "%n## 경로 기준%n1. 최단 거리%n2. 최단시간%nB. 돌아가기%n";
+    private static final String START_STATION = "%n## 출발역을 입력하세요.%n";
+    private static final String END_STATION = "%n## 도착역을 입력하세요.%n";
 
     public void printMain() {
         System.out.printf(MAIN);
@@ -15,5 +17,13 @@ public class Output {
 
     public void printStandard() {
         System.out.printf(STANDARD);
+    }
+
+    public void askStartStation() {
+        System.out.printf(START_STATION);
+    }
+
+    public void askEndStation() {
+        System.out.printf(END_STATION);
     }
 }
