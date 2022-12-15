89e0dcee0db194185ce18453ff936f6b4943fa02
anneu <b.imnuj@gmail.com>
2022-12-15 17:10:22 +0900

    feat: 기능 선택 요청 메세지를 출력하는 기능 구현

--- subway-path/src/main/java/subway/veiw/Output.java
+++ subway-path/src/main/java/subway/veiw/Output.java
@@ -2,8 +2,13 @@ package subway.veiw;
 
 public class Output {
     private static final String MAIN = "## 메인 화면%n1. 경로 조회%nQ. 종료%n";
+    private static final String CHOICE = "%n## 원하는 기능을 선택하세요.%n";
 
     public void printMain() {
         System.out.printf(MAIN);
+    }
+
+    public void askChoice() {
+        System.out.printf(CHOICE);
     }
 }
