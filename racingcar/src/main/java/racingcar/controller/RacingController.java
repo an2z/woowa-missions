c3b9d151f72441f9b1ef0716bbad0a3bd3a70945
anneu <b.imnuj@gmail.com>
2023-02-08 16:46:45 +0900

    feat: 자동차를 만드는 기능 구현

--- racingcar/src/main/java/racingcar/controller/RacingController.java
+++ racingcar/src/main/java/racingcar/controller/RacingController.java
@@ -3,9 +3,25 @@ package racingcar.controller;
 import static racingcar.view.Input.readCarNames;
 import static racingcar.view.Output.printCarNames;
 
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.domain.Car;
+import racingcar.domain.Cars;
+
 public class RacingController {
+
     public void run() {
         printCarNames();
-        readCarNames();
+        Cars cars = makeCars(readCarNames());
+    }
+
+    private static Cars makeCars(String[] carNames) {
+        List<Car> cars = new ArrayList<>();
+
+        for (String carName : carNames) {
+            cars.add(new Car(carName));
+        }
+
+        return new Cars(cars);
     }
 }
---
+++ racingcar/src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,36 @@
+package racingcar.domain;
+
+import java.util.Objects;
+
+public class Car {
+    private final String name;
+    private int position = 0;
+
+    public Car(String name) {
+        validateNameLength(name);
+        this.name = name;
+    }
+
+    private void validateNameLength(String name) {
+        if (name.trim().length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 5글자 이하만 가능합니다.");
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Car car = (Car) o;
+        return position == car.position && Objects.equals(name, car.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, position);
+    }
+}
