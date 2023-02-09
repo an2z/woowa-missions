942531c3448ffc951bed1710beb1f0375d158c40
anneu <b.imnuj@gmail.com>
2023-02-09 15:43:49 +0900

    refactor: 상수 분리

--- racingcar/src/main/java/racingcar/view/Input.java
+++ racingcar/src/main/java/racingcar/view/Input.java
@@ -3,8 +3,9 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Input {
+    private static final int MIN_COUNT = 1;
     private static final String REGEX = ",";
-    private static final String COUNT_ERROR_MESSAGE = "최소 1번 이상 경주를 진행해야 합니다.";
+    private static final String COUNT_ERROR_MESSAGE = "최소 %d번 이상 경주를 진행해야 합니다.";
 
     public static String[] readCarNames() {
         return read().split(REGEX);
@@ -15,8 +16,8 @@ public class Input {
     }
 
     private static int checkCount(int count) {
-        if (count < 1) {
-            throw new IllegalArgumentException(COUNT_ERROR_MESSAGE);
+        if (count < MIN_COUNT) {
+            throw new IllegalArgumentException(String.format(COUNT_ERROR_MESSAGE, MIN_COUNT));
         }
         return count;
     }
