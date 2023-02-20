901c7b232774c80bc5b48a3acd6651e4ee010d34
anneu <b.imnuj@gmail.com>
2023-02-20 14:27:06 +0900

    refactor: 서비스 클래스 분리

--- ladder/src/main/java/controller/LadderController.java
+++ ladder/src/main/java/controller/LadderController.java
@@ -1,27 +1,21 @@
 package controller;
 
-import static java.util.stream.Collectors.toList;
-
-import domain.LadderHeight;
-import domain.Player;
-import domain.Players;
-import java.util.List;
+import domain.LadderService;
 import view.Input;
 import view.Output;
 
 public class LadderController {
-    public void run() {
-        Output.printStartMessage();
-        List<String> names = Input.readNames();
-        Players players = new Players(makePlayer(names));
+    private final LadderService ladderService;
 
-        Output.printLadderHeightMessage();
-        LadderHeight ladderHeight = new LadderHeight(Input.readLadderHeight());
+    public LadderController() {
+        this.ladderService = new LadderService();
     }
 
-    private static List<Player> makePlayer(List<String> names) {
-        return names.stream()
-                .map(Player::new)
-                .collect(toList());
+    public void run() {
+        Output.printStartMessage();
+        ladderService.makePlayers(Input.readNames());
+
+        Output.printLadderHeightMessage();
+        ladderService.makeHeight(Input.readLadderHeight());
     }
 }
---
+++ ladder/src/main/java/domain/LadderService.java
@@ -0,0 +1,24 @@
+package domain;
+
+import static java.util.stream.Collectors.toList;
+
+import java.util.List;
+
+public class LadderService {
+    private Players players;
+    private LadderHeight ladderHeight;
+
+    public void makeHeight(int height) {
+        ladderHeight = new LadderHeight(height);
+    }
+
+    public void makePlayers(List<String> names) {
+        players = new Players(makePlayer(names));
+    }
+
+    private List<Player> makePlayer(List<String> names) {
+        return names.stream()
+                .map(Player::new)
+                .collect(toList());
+    }
+}
