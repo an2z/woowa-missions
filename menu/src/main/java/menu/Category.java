7b78f197e720618994320b18631199d9bdc5cffa
anneu <b.imnuj@gmail.com>
2023-08-09 01:07:56 +0900

    feat: 랜덤으로 구해진 번호에 해당하는 카테고리를 조회하는 기능 구현

--- menu/src/main/java/menu/Category.java
+++ menu/src/main/java/menu/Category.java
@@ -1,5 +1,6 @@
 package menu;
 
+import java.util.Arrays;
 import java.util.List;
 
 public enum Category {
@@ -10,6 +11,8 @@ public enum Category {
     ASIAN("아시안", 4, List.of("팟타이", "카오 팟", "나시고렝", "파인애플 볶음밥", "쌀국수", "똠얌꿍", "반미", "월남쌈", "분짜")),
     WESTERN("양식", 5, List.of("라자냐", "그라탱", "뇨끼", "끼슈", "프렌치 토스트", "바게트", "스파게티", "피자", "파니니"));
 
+    public static final String NOT_FOUND_CATEGORY = "[ERROR] 카테고리를 찾을 수 없습니다.";
+
     private final int key;
     private final String name;
     private final List<String> menus;
@@ -18,5 +21,12 @@ public enum Category {
         this.name = name;
         this.key = key;
         this.menus = menus;
+    }
+
+    public static Category find(int key) {
+        return Arrays.stream(values())
+                .filter(category -> category.key == key)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(NOT_FOUND_CATEGORY));
     }
 }
