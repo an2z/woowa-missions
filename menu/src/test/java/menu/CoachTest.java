4d9ab973812914174e990c0b30a7149b5dd67ea9
anneu <b.imnuj@gmail.com>
2023-08-09 01:02:10 +0900

    test: 코치가 싫어하는 메뉴가 0~2개가 아닐 경우 예외 발생 테스트 작성

--- menu/src/test/java/menu/CoachTest.java
+++ menu/src/test/java/menu/CoachTest.java
@@ -1,6 +1,7 @@
 package menu;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -13,6 +14,19 @@ class CoachTest {
     @CsvSource({"일", "일이삼사오"})
     void makeCoachEx(String name) {
         assertThatThrownBy(() -> new Coach(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageStartingWith("[ERROR]");
+    }
+
+    @DisplayName("코치의 싫어하는 메뉴가 0~2개가 아닐 경우 예외가 발생한다.")
+    @Test
+    void addHateMenuEx() {
+        // given
+        Coach coach = new Coach("토미");
+        String[] hateMenu = new String[]{"우동", "스시", "비빔밥"};
+
+        // when, then
+        assertThatThrownBy(() -> coach.addHateMenu(hateMenu))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageStartingWith("[ERROR]");
     }
