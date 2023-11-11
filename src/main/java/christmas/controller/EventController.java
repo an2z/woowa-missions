07f8d3a888593d6d1f6e867501e761bb0f5b4d79
anneu <b.imnuj@gmail.com>
2023-11-11 12:03:50 +0900

    feat: 사용자 입력 값으로 방문 날짜와 주문 메뉴 목록을 만드는 기능 구현

--- src/main/java/christmas/controller/EventController.java
+++ src/main/java/christmas/controller/EventController.java
@@ -1,15 +1,35 @@
 package christmas.controller;
 
+import christmas.domain.Date;
+import christmas.domain.OrderMenu;
+import christmas.domain.OrderMenus;
 import christmas.view.InputView;
 import christmas.view.OutputView;
 
+import java.util.List;
+
 public class EventController {
+    private static final String ORDER_INFO_SEPARATOR = "-";
+
     private final OutputView outputView = new OutputView();
     private final InputView inputView = new InputView();
 
     public void run() {
         outputView.showGreeting();
-        inputView.readDate();
-        inputView.readOrderMenus();
+        Date date = new Date(inputView.readDate());
+        OrderMenus orderMenus = makeOrderMenus(inputView.readOrderMenus());
+    }
+
+    private OrderMenus makeOrderMenus(List<String> input) {
+        return new OrderMenus(makeOrderMenu(input));
+    }
+
+    private List<OrderMenu> makeOrderMenu(List<String> input) {
+        return input.stream()
+                .map(menu -> {
+                    String[] result = menu.split(ORDER_INFO_SEPARATOR);
+                    return OrderMenu.from(result[0], Integer.parseInt(result[1]));
+                })
+                .toList();
     }
 }
