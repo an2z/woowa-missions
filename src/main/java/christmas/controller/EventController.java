3ad89fc92af280847bdfb4b1ca3e5176d3692466
anneu <b.imnuj@gmail.com>
2023-11-11 11:22:15 +0900

    feat: 방문 날짜 입력 기능 구현

--- src/main/java/christmas/controller/EventController.java
+++ src/main/java/christmas/controller/EventController.java
@@ -1,11 +1,14 @@
 package christmas.controller;
 
+import christmas.view.InputView;
 import christmas.view.OutputView;
 
 public class EventController {
     private final OutputView outputView = new OutputView();
+    private final InputView inputView = new InputView();
 
     public void run() {
         outputView.showGreeting();
+        inputView.readDate();
     }
 }
