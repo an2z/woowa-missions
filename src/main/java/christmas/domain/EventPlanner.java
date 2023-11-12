cf5accd62d655d9c9bcf67c93ada80e04fe312c6
anneu <b.imnuj@gmail.com>
2023-11-12 16:31:04 +0900

    feat: 혜택 내역을 구하는 기능 구현

--- src/main/java/christmas/domain/EventPlanner.java
+++ src/main/java/christmas/domain/EventPlanner.java
@@ -1,12 +1,25 @@
 package christmas.domain;
 
 import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class EventPlanner {
     private final EventStore eventStore = new EventStore();
 
-    public List<Event> findOngoingEvents(Reservation reservation) {
+    public Map<Event, Integer> findBenefitsByEvent(Reservation reservation) {
+        Map<Event, Integer> benefitsByEvent = new HashMap<>();
+        for (Event event : findOngoingEvents(reservation)) {
+            int discount = event.calculateDiscount(reservation);
+            if (discount > 0) {
+                benefitsByEvent.put(event, discount);
+            }
+        }
+        return benefitsByEvent;
+    }
+
+    private List<Event> findOngoingEvents(Reservation reservation) {
         LocalDate visitDate = LocalDate.of(2023, 12, reservation.getVisitDate().getDay());
         return eventStore.findOngoingEvents(visitDate);
     }
