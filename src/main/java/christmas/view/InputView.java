e558401a7e9b31d86bddbfc3598fd9fc984f9f3d
anneu <b.imnuj@gmail.com>
2023-11-13 14:26:38 +0900

    refactor: 주문 메뉴 입력 값 변환 로직을 InputViewConverter 클래스로 분리

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -2,11 +2,9 @@ package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class InputView {
-    private static final String SEPARATOR = ",";
     private static final String DATE_INPUT_MESSAGE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
     private static final String ORDER_INFO_INPUT_MESSAGE = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
 
@@ -24,11 +22,7 @@ public class InputView {
         System.out.println(ORDER_INFO_INPUT_MESSAGE);
         String input = read();
         validator.validateOrderInfo(input);
-        return convert(input);
-    }
-
-    private List<String> convert(String input) {
-        return Arrays.stream(input.split(SEPARATOR)).toList();
+        return convertor.convertToOrderInfo(input);
     }
 
     private String read() {
--- src/main/java/christmas/view/InputViewConvertor.java
+++ src/main/java/christmas/view/InputViewConvertor.java
@@ -1,7 +1,16 @@
 package christmas.view;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class InputViewConvertor {
+    private static final String SEPARATOR = ",";
+
     public int convertToDate(String input) {
         return Integer.parseInt(input);
+    }
+
+    public List<String> convertToOrderInfo(String input) {
+        return Arrays.stream(input.split(SEPARATOR)).toList();
     }
 }
