3ca6c73166721baaceca06ac12104f7f890e3214
anneu <b.imnuj@gmail.com>
2023-11-15 01:51:01 +0900

    test: 방문 날짜가 별이 있는 날짜에 해당하는지 확인하는 기능 테스트 추가

--- src/test/java/christmas/domain/VisitDateTest.java
+++ src/test/java/christmas/domain/VisitDateTest.java
@@ -2,8 +2,12 @@ package christmas.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class VisitDateTest {
@@ -13,5 +17,20 @@ class VisitDateTest {
     void wrongDayEx(int day) {
         assertThatThrownBy(() -> VisitDate.from(day))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("방문 날짜가 별이 있는 날짜에 해당하는지 확인한다.")
+    @CsvSource({
+            "3, true",
+            "10, true",
+            "17, true",
+            "4, false",
+            "5, false"
+    })
+    @ParameterizedTest
+    void checkSpecialDay(int day, boolean expected) {
+        VisitDate visitDate = VisitDate.from(day);
+        boolean result = visitDate.isSpecialDay(List.of(3, 10, 17, 24, 25, 31));
+        assertThat(result).isEqualTo(expected);
     }
 }
