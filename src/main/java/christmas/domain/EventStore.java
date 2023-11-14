6d6bf9ce9cdca0e866e6b329470c3cfdbce137d8
anneu <b.imnuj@gmail.com>
2023-11-14 23:17:37 +0900

    refactor: 이벤트 저장소 초기화 방법 변경 및 날짜 값을 상수화

--- src/main/java/christmas/domain/EventStore.java
+++ src/main/java/christmas/domain/EventStore.java
@@ -1,20 +1,27 @@
 package christmas.domain;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
 public class EventStore {
-    private List<Event> events = new ArrayList<>();
+    private static final int YEAR = 2023;
+    private static final int MONTH = 12;
+    private static final int FIRST_DAY = 1;
+    private static final int LAST_DAY = 31;
+    private static final int CHRISTMAS_DAY = 25;
+    private static final LocalDate START_DATE = LocalDate.of(YEAR, MONTH, FIRST_DAY);
+    private static final LocalDate END_DATE = LocalDate.of(YEAR, MONTH, LAST_DAY);
+    private static final LocalDate CHRISTMAS_DATE = LocalDate.of(YEAR, MONTH, CHRISTMAS_DAY);
+
+    private final List<Event> events;
 
     public EventStore() {
-        LocalDate lastDate = LocalDate.of(2023, 12, 31);
-        LocalDate christmasDate = LocalDate.of(2023, 12, 25);
-        LocalDate startDate = LocalDate.of(2023, 12, 1);
-        events.add(new ChristmasEvent(startDate, christmasDate));
-        events.add(new WeekEvent(startDate, lastDate));
-        events.add(new SpecialEvent(startDate, lastDate));
-        events.add(new GiftEvent(startDate, lastDate));
+        this.events = List.of(
+                new ChristmasEvent(START_DATE, CHRISTMAS_DATE),
+                new WeekEvent(START_DATE, END_DATE),
+                new SpecialEvent(START_DATE, END_DATE),
+                new GiftEvent(START_DATE, END_DATE)
+        );
     }
 
     public List<Event> findOngoingEvents(LocalDate visitDate) {
