4f8e55435b264dec1d3ae6c3fe8ac8296f2ad5b7
anneu <b.imnuj@gmail.com>
2023-11-15 10:47:11 +0900

    test: 총 주문금액에 따른 증정 이벤트의 할인 금액 계산 기능 테스트 추가

--- src/test/java/christmas/domain/GiftEventTest.java
+++ src/test/java/christmas/domain/GiftEventTest.java
@@ -1,8 +1,15 @@
 package christmas.domain;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.time.LocalDate;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class GiftEventTest {
     GiftEvent giftEvent;
@@ -13,5 +20,35 @@ class GiftEventTest {
                 LocalDate.of(2023, 12, 1),
                 LocalDate.of(2023, 12, 31)
         );
+    }
+
+    @DisplayName("방문 날짜가 증정 이벤트 진행 기간인지 확인한다.")
+    @ValueSource(ints = {1, 25, 31})
+    @ParameterizedTest
+    void checkPeriod(int day) {
+        LocalDate visitDate = LocalDate.of(2023, 12, day);
+        boolean result = giftEvent.isDateWithinPeriod(visitDate);
+        assertThat(result).isTrue();
+    }
+
+    @DisplayName("총 주문금액이 120000원 이상일 경우 증정메뉴 가격만큼 할인된다.")
+    @CsvSource({
+            "1, 0",
+            "2, 0",
+            "3, 25000",
+            "10, 25000"
+    })
+    @ParameterizedTest
+    void calculateDiscount(int count, int expected) {
+        //given
+        VisitDate visitDate = VisitDate.makeDecemberVisitDate(1);
+        Orders orders = new Orders(List.of(new Order(Menu.BBQ_RIBS, count)));
+        Reservation reservation = new Reservation(visitDate, orders);
+
+        //when
+        int discount = giftEvent.calculateDiscount(reservation);
+
+        //then
+        assertThat(discount).isEqualTo(expected);
     }
 }
