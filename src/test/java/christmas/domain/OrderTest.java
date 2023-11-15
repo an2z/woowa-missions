950d57b797742cdd604ae65d739e65b2022d35bf
anneu <b.imnuj@gmail.com>
2023-11-15 17:16:23 +0900

    test: 주문 정보 검증 예외 테스트 추가

--- src/test/java/christmas/domain/OrderTest.java
+++ src/test/java/christmas/domain/OrderTest.java
@@ -2,6 +2,11 @@ package christmas.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -11,5 +16,22 @@ class OrderTest {
     void validateCount() {
         assertThatThrownBy(() -> new Order(Menu.BBQ_RIBS, 0))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("올바르지 않은 주문 정보일 경우 예외가 발생한다.")
+    @MethodSource("provideInvalidOrderInfo")
+    @ParameterizedTest
+    void validateOrderInfo(String[] orderInfo) {
+        assertThatThrownBy(() -> Order.from(orderInfo))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private static Stream<Arguments> provideInvalidOrderInfo() {
+        return Stream.of(
+                Arguments.of((Object) new String[]{"해산물파스타1"}),
+                Arguments.of((Object) new String[]{"초코케이크_1"}),
+                Arguments.of((Object) new String[]{"레드와인", "ㄱ"}),
+                Arguments.of((Object) new String[]{"레드와인", "!"})
+        );
     }
 }
