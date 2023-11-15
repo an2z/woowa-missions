dea8444f17e9d8bc38d5f4a76db51f2f1e0069a6
anneu <b.imnuj@gmail.com>
2023-11-15 10:27:40 +0900

    chore: 방문 날짜 검증 테스트 이름 변경

--- src/test/java/christmas/domain/VisitDateTest.java
+++ src/test/java/christmas/domain/VisitDateTest.java
@@ -12,10 +12,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class VisitDateTest {
-    @DisplayName("날짜가 1일 ~ 31일이 아닐 경우 예외가 발생한다.")
+    @DisplayName("방문 날짜가 1일 ~ 31일이 아닐 경우 예외가 발생한다.")
     @ValueSource(ints = {0, 32, 33, 99})
     @ParameterizedTest
-    void wrongDayEx(int day) {
+    void makeInvalidVisitDate(int day) {
         assertThatThrownBy(() -> VisitDate.makeDecemberVisitDate(day))
                 .isInstanceOf(IllegalArgumentException.class);
     }
