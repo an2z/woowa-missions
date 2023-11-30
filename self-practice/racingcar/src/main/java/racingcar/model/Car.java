2969f8af22d329c4657581d3f9c5019b94f35075
anneu <b.imnuj@gmail.com>
2023-11-30 11:37:03 +0900

    feat: 자동차 이름 검증 기능 구현

--- self-practice/racingcar/src/main/java/racingcar/model/Car.java
+++ self-practice/racingcar/src/main/java/racingcar/model/Car.java
@@ -7,7 +7,14 @@ public class Car {
     private int movement;
 
     public Car(String name) {
+        validate(name);
         this.name = name;
+    }
+
+    private void validate(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다.");
+        }
     }
 
     public void move(int condition) {
