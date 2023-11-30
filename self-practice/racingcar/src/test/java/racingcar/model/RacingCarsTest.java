90ed1852b2674f4db3695140f46f9bbe6f7286ca
anneu <b.imnuj@gmail.com>
2023-11-30 12:25:03 +0900

    test: 랜덤 값이 4 이상일 때 자동차가 전진하는 기능 테스트 작성

--- self-practice/racingcar/src/test/java/racingcar/model/RacingCarsTest.java
+++ self-practice/racingcar/src/test/java/racingcar/model/RacingCarsTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class RacingCarsTest {
@@ -21,5 +22,24 @@ class RacingCarsTest {
         Car pobiCar = new Car("pobi");
         assertThatThrownBy(() -> new RacingCars(List.of(pobiCar, pobiCar)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("랜덤 값이 4 이상일 경우 자동차가 전진한다.")
+    @Test
+    void race() {
+        // given
+        Car pobi = new Car("pobi");
+        Car wori = new Car("wori");
+        Car jun = new Car("jun");
+        RacingCars racingCars = new RacingCars(List.of(pobi, wori, jun));
+
+        // when
+        List<Integer> randomNumbers = List.of(1, 2, 4);
+        racingCars.race(randomNumbers);
+
+        // then
+        assertThat(pobi.getMovement()).isZero();
+        assertThat(wori.getMovement()).isZero();
+        assertThat(jun.getMovement()).isEqualTo(1);
     }
 }
