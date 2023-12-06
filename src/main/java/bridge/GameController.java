5243cd5a8972d6d0ee59f63e5430a30469cb3753
anneu <b.imnuj@gmail.com>
2023-12-06 16:30:01 +0900

    feat: 예외 발생 시 에러 메세지 출력 후 재입력 받는 기능 구현

--- src/main/java/bridge/GameController.java
+++ src/main/java/bridge/GameController.java
@@ -1,20 +1,40 @@
 package bridge;
 
+import bridge.domain.Bridge;
 import bridge.view.InputView;
 import bridge.view.OutputView;
+
+import java.util.function.Supplier;
 
 public class GameController {
 
     private final InputView inputView;
     private final OutputView outputView;
+    private final BridgeMaker bridgeMaker;
 
     public GameController() {
         this.inputView = new InputView();
         this.outputView = new OutputView();
+        this.bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
     }
 
     public void run() {
         outputView.printStartMessage();
-        inputView.readBridgeSize();
+        Bridge bridge = retry(this::makeBridge);
+    }
+
+    private Bridge makeBridge() {
+        int size = inputView.readBridgeSize();
+        return bridgeMaker.makeBridge(size);
+    }
+
+    public <T> T retry(Supplier<T> supplier) {
+        while (true) {
+            try {
+                return supplier.get();
+            } catch (IllegalArgumentException e) {
+                outputView.printErrorMessage(e.getMessage());
+            }
+        }
     }
 }
--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -11,6 +11,10 @@ public class OutputView {
         System.out.println("다리 건너기 게임을 시작합니다." + LINE_SEPARATOR);
     }
 
+    public void printErrorMessage(String message) {
+        System.out.println("[ERROR] " + message);
+    }
+
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
