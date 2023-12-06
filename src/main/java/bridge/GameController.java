c8691bc8a73d56549b922c3b7862227093f5e244
anneu <b.imnuj@gmail.com>
2023-12-06 16:14:57 +0900

    feat: 컨트롤러 클래스 생성

---
+++ src/main/java/bridge/GameController.java
@@ -0,0 +1,16 @@
+package bridge;
+
+public class GameController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public GameController() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+    }
+
+    public void run() {
+        //TODO: 기능 구현
+    }
+}
