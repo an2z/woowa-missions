62f79c9c3da01ff51dc3887874b3f6ee0cd56673
anneu <b.imnuj@gmail.com>
2023-12-06 16:09:50 +0900

    feat: 다리 길이 검증 기능 구현

--- src/main/java/bridge/domain/Bridge.java
+++ src/main/java/bridge/domain/Bridge.java
@@ -7,6 +7,13 @@ public class Bridge {
     private final List<String> steps;
 
     public Bridge(List<String> steps) {
+        validateSize(steps);
         this.steps = steps;
+    }
+
+    private void validateSize(List<String> steps) {
+        if (steps.size() < 3 || steps.size() > 20) {
+            throw new IllegalArgumentException("다리 길이는 3 이상 20 이하여야 합니다.");
+        }
     }
 }
